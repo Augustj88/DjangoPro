25ce9d492d9fab304835775549cb909e5a61ab6d
EUGENE\유진 <yjjung1017@naver.com>
2022-02-11 15:33:37 +0900

    add test

--- DjangoPro/urls.py
+++ DjangoPro/urls.py
@@ -14,14 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpRequest
 from django.urls import path
 from ninja import NinjaAPI  # 장고닌자 import!
+from typing import Dict
 
 api = NinjaAPI()  # 장고닌자 api를 바로 인스턴스화
 
 
-@api.get("/add")  # 함수를 데코레이터로 감싸서 사용
-def add(request, a: int, b: int):  # view function을 데코레이터로 감싸고 /add 로 url을 넘겨준다
+@api.get("/add")  # 함수를 데코레이터로 감싸서 사용 / # view function을 데코레이터로 감싸고 /add 로 url을 넘겨준다
+def add(request: HttpRequest, a: int, b: int) -> Dict[str, int]:
     return {"result": a + b}  # 기존 장고의 분리된 방식보다 가독성 up!!
 
 
@@ -29,7 +31,3 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", api.urls),
 ]
-
-print(
-    "Life is Toooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo Short"
-)
